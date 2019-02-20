8e9a65ea47357cdebe87ce6b5dbc1ffe569232a5
Dan Kennedy <dannykennedy@email.com>
2019-02-20 16:12:01 -0500

    trying a db connection

--- socialnotes-be/server/routes/users/index.js
+++ socialnotes-be/server/routes/users/index.js
@@ -2,10 +2,21 @@
 
 const express = require('express');
 const router = express.Router();
+const con =
 
 module.exports = () => {
 
     router.get('/', (req, resp, next) => {
+
+        con.query('SELECT * from Users',
+            function(err,rows,fields) {
+                if (err)
+                    console.log('Error during query processing');
+                else
+                    console.log('Here is the result : ', rows);
+// See next slide for an alternate way to parse
+//and display the result
+            });
         return resp.send(`I guessing this is the route where you type notes?`);
     });
 
