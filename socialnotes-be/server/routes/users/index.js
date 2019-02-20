48658569f25fea7ade114fc811ee93220f68ad59
root <root@tagnostic.localdomain>
2019-02-20 20:24:56 +0000

    commit from server

--- socialnotes-be/server/routes/users/index.js
+++ socialnotes-be/server/routes/users/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 module.exports = () => {
 
     router.get('/', (req, resp, next) => {
-        return resp.send(`I guess this is the route where you type notes?`);
+        return resp.send(`I guessing this is the route where you type notes?`);
     });
 
     router.get('/:userName', (req, resp, next) => {
