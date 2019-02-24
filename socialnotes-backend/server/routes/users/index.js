232941e3d41b4a6552862a2da83689eb1ea1da61
Dan Kennedy <dannykennedy@email.com>
2019-02-24 15:33:37 -0500

    cors

--- socialnotes-backend/server/routes/users/index.js
+++ socialnotes-backend/server/routes/users/index.js
@@ -3,10 +3,11 @@
 const express = require('express');
 const router = express.Router();
 const dbUserFunctions = require("../../database_functions/user_functions.js");
+const cors = require('cors');
 
 module.exports = () => {
 
-    router.get('/', (req, resp, next) => {
+    router.get('/', cors(), (req, resp, next) => {
 
         dbUserFunctions.getUsersByLastName("Chang", function (err, results) {
             if(err) { resp.send(500,"Server Error"); return;}
