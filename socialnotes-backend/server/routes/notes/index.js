39e05f894c397ba386b2525f690411f6d3d8a408
Dan Kennedy <dannykennedy@email.com>
2019-03-07 20:03:28 -0500

    new notes functions

--- socialnotes-backend/server/routes/notes/index.js
+++ socialnotes-backend/server/routes/notes/index.js
@@ -2,14 +2,22 @@
 
 const express = require('express');
 const router = express.Router();
+const dbNoteFunctions = require("../../database_functions/note_functions.js");
 
 module.exports = () => {
-    router.get('/', (req, res, next) => {
-        return res.send('this is ya notes');
+    router.get('/', (req, resp, next) => {
+        return resp.send('this is ya notes');
     });
 
-    router.get('/:userNumber', (req, res, next) => {
-        return res.send(`I'm going to send you the notes of user ${req.params.userNumber}`);
+    router.get('/:userId', (req, resp, next) => {
+
+        const id = req.params.userId;
+        console.log(req.params.userId);
+
+        dbNoteFunctions.getNotesByUser(id, function (err, results) {
+            if(err) { resp.send(500,"Server Error"); return;}
+            resp.send(results);
+        });
     });
 
     return router;
