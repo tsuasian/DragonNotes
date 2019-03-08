1e9f8ed96d6a5a070b83aa4858f2edbfafd55154
Dan Kennedy <dannykennedy@email.com>
2019-03-08 13:00:49 -0500

    addNote function

--- socialnotes-backend/server/database_functions/note_functions.js
+++ socialnotes-backend/server/database_functions/note_functions.js
@@ -1,5 +1,7 @@
 'use strict';
 const db = require('../db.js');
+const uuidv4 = require('uuid/v4');
+const bodyparser = require('body-parser');
 
 // Example functions
 exports.getNotesByUser = (userId, callback) => {
@@ -20,6 +22,24 @@ exports.getNotesByUser = (userId, callback) => {
 exports.getNotesInGroup = (groupId, callback) => {
 
     const sql = "select * from Notes join NotesGroups on Notes.noteId LIKE NotesGroups.noteId where NotesGroups.groupId LIKE \'" + groupId + "\'";
+
+    // get a connection from the pool
+    db.getConnection(function(err, connection) {
+        if(err) { console.log(err); callback(true); return; }
+        // make the query
+        connection.query(sql, function(err, results) {
+            connection.release();
+            if(err) { console.log(err); callback(true); return; }
+            callback(false, results);
+        });
+    });
+};
+
+exports.addNote = (userId, noteText, callback) => {
+
+    const noteId = uuidv4();
+    const datetime = new Date().toLocaleString();
+    const sql = `INSERT INTO Notes VALUES ('${noteId}', '${noteText}', ${userId}, '${datetime}','${datetime}')`;
 
     // get a connection from the pool
     db.getConnection(function(err, connection) {
--- socialnotes-backend/server/routes/notes/index.js
+++ socialnotes-backend/server/routes/notes/index.js
@@ -3,11 +3,13 @@
 const express = require('express');
 const router = express.Router();
 const dbNoteFunctions = require("../../database_functions/note_functions.js");
+const cors = require('cors');
+// const bodyParser = require("body-parser");
+
 
 module.exports = () => {
-    router.get('/', (req, resp, next) => {
-        return resp.send('this is ya notes');
-    });
+
+    router.use(cors());
 
     router.get('/:userId', (req, resp, next) => {
 
@@ -17,6 +19,22 @@ module.exports = () => {
         dbNoteFunctions.getNotesByUser(id, function (err, results) {
             if(err) { resp.send(500,"Server Error"); return;}
             resp.send(results);
+        });
+    });
+
+    // POST a new note.
+    // Body of requests should look like this:
+    // {
+    //     "postText": "my name is Timbo Jones",
+    //     "userId": 1
+    // }
+    router.post('/', cors(), (req, res, next) => {
+        let post = req.body;
+        let postText = post.postText;
+        let userId = post.userId;
+        dbNoteFunctions.addNote(userId, postText, function (err, results) {
+            if(err) { res.send(500,"Server Error"); return;}
+            res.send(results);
         });
     });
 
