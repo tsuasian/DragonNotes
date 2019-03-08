a52e70e0254c0f0d4595f0500a299f6020d1ecdc
Dan Kennedy <dannykennedy@email.com>
2019-03-07 20:02:28 -0500

    cleanup

---
+++ socialnotes-backend/server/database_functions/notes_functions.js
@@ -0,0 +1,34 @@
+'use strict';
+const db = require('../db.js');
+
+// Example functions
+exports.getNotesByUser = (userId, callback) => {
+
+    const sql = "SELECT * FROM Notes WHERE postedBy=?";
+    // get a connection from the pool
+    db.getConnection(function(err, connection) {
+        if(err) { console.log(err); callback(true); return; }
+        // make the query
+        connection.query(sql, [userId], function(err, results) {
+            connection.release();
+            if(err) { console.log(err); callback(true); return; }
+            callback(false, results);
+        });
+    });
+};
+
+exports.getNotesInGroup = (groupId, callback) => {
+
+     const sql = "select * from Notes join NotesGroups on Notes.noteId LIKE NotesGroups.noteId where NotesGroups.groupId LIKE \'" + groupId + "\'";
+
+    // get a connection from the pool
+    db.getConnection(function(err, connection) {
+        if(err) { console.log(err); callback(true); return; }
+        // make the query
+        connection.query(sql, function(err, results) {
+            connection.release();
+            if(err) { console.log(err); callback(true); return; }
+            callback(false, results);
+        });
+    });
+};
--- socialnotes-backend/server/routes/notes/index.js
+++ socialnotes-backend/server/routes/notes/index.js
@@ -18,36 +18,7 @@ module.exports = () => {
             if(err) { resp.send(500,"Server Error"); return;}
             resp.send(results);
         });
-
-        // return resp.send(`I'm going to send you the notes of user ${req.params.userName}`);
     });
 
     return router;
 };
-
-/*
-'use strict';
-
-const express = require('express');
-const router = express.Router();
-const dbUserFunctions = require("../../database_functions/user_functions.js");
-const cors = require('cors');
-
-module.exports = () => {
-
-    router.get('/', cors(), (req, resp, next) => {
-
-        dbUserFunctions.getUsersByLastName("Kennedy", function (err, results) {
-            if(err) { resp.send(500,"Server Error"); return;}
-            resp.send(results);
-        });
-    });
-
-    router.get('/:userName', cors(), (req, resp, next) => {
-        return resp.send(`This is the profile page for ${req.params.userName}`);
-    });
-
-    return router;
-};
-
-*/
