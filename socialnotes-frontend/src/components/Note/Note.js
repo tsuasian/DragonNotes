708e5acdbb37c7f5ddbdef9ca73f8dff7f2e3133
Dan Kennedy <dannykennedy@email.com>
2019-03-15 11:47:59 -0400

    Make note a class

--- socialnotes-frontend/src/components/Note/Note.js
+++ socialnotes-frontend/src/components/Note/Note.js
@@ -2,39 +2,55 @@ import React from 'react'
 import '../css/note.css'
 import {Card, CardContent, CardHeader, Avatar, IconButton} from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import ShareIcon from '@material-ui/icons/Share';
 import CardActions from "@material-ui/core/CardActions/CardActions";
 import ShareModal from "../ShareDialog/ShareDialog"
 
-export const Note = ({noteText="-", postedBy= "-", timePosted="-", edited, groups}) => {
-    return (
-        <Card className={'noteCard'}>
-            <CardHeader
-                avatar={
-                    <Avatar aria-label="Note" style={{backgroundColor: '#f44336'}} className={'red'}>
-                        {postedBy}
-                    </Avatar>
-                }
-                action={
-                    <IconButton>
-                        <MoreVertIcon />
-                    </IconButton>
-                }
-                title={noteText.split(' ')[0]}
-                subheader={timePosted}
-            />
-            <CardContent>
-            <p>{noteText}</p>
-            <p>{edited ? 'edited': ''}</p>
-            </CardContent>
+export class Note extends React.Component {
+    state = {};
 
-            <CardActions className={'actions'} disableActionSpacing>
-                {/*<IconButton aria-label="Share">*/}
+    render() {
+
+        const { noteText } = this.props;
+        const { groups } = this.props;
+        const { postedBy } = this.props;
+        const { timePosted } = this.props;
+        const { edited } = this.props;
+
+        return (
+            <Card className={'noteCard'}>
+                <CardHeader
+                    avatar={
+                        <Avatar aria-label="Note" style={{backgroundColor: '#f44336'}} className={'red'}>
+                            {postedBy}
+                        </Avatar>
+                    }
+                    action={
+                        <IconButton
+                            // aria-owns={isMenuOpen ? 'material-appbar' : undefined}
+                            aria-haspopup="true"
+                            // onClick={this.handleProfileMenuOpen}
+                            color="inherit"
+                        >
+                            <MoreVertIcon />
+                        </IconButton>
+                    }
+                    title={noteText.split(' ')[0]}
+                    subheader={timePosted}
+                />
+                <CardContent>
+                    <p>{noteText}</p>
+                    <p>{edited ? 'edited': ''}</p>
+                </CardContent>
+
+                <CardActions className={'actions'} disableActionSpacing>
+                    {/*<IconButton aria-label="Share">*/}
                     {/*<ShareIcon />*/}
-                {/*</IconButton>*/}
-                <ShareModal groups={groups}/>
-            </CardActions>
+                    {/*</IconButton>*/}
+                    <ShareModal groups={groups}/>
+                </CardActions>
 
-        </Card>
-    )
-};
+            </Card>
+        )
+
+    }
+}
